e5a918869c168ee32b9c9fd1bb1f26dfb9a9d523
Vawdj <vawdreyjayden14@gmail.com>
2025-04-05 15:57:11 +0200

    added switch functions, added cyclic reference support

--- switch.lua
+++ switch.lua
@@ -1,51 +1,100 @@
-local function tbl_eq(t1, t2)
-    if t1 == t2 then return true end
-
-    local l1, l2 = 0, 0
-
-    for k, v1 in pairs(t1) do
-        if not t2[k] then return false end
-
-        local typ1, v2 = type(v1), t2[k]
-
-        if typ1 ~= type(v2) then return false end
-
-        if typ1 == "table" then
-            if not tbl_eq(v1, v2) then return false end
-        else
-            if v1 ~= v2 then return false end
+local function is_eq(v1, v2, visited)
+    if v1 == v2 then return true end
+    local t1 = type(v1)
+    if t1 ~= type(v2) then return false end
+    if t1 == "table" then
+        visited = visited or {}
+        local pair_key = tostring(v1) .. ":" .. tostring(v2)
+        if visited[pair_key] then return true end
+        visited[pair_key] = true
+        local l1, l2 = 0, 0
+        for k, i1 in pairs(v1) do
+            if v2[k] == nil then return false end
+            if not is_eq(i1, v2[k], visited) then return false end
+            l1 = l1 + 1
         end
-
-        l1 = l1 + 1
+        for _ in pairs(v2) do l2 = l2 + 1 end
+        return l1 == l2
     end
-
-    for _ in pairs(t2) do
-        l2 = l2 + 1
-    end
-
-    return l1 == l2
+    return false
 end
 
-return function(v)
+local switch = { id = {} }
+
+function switch.list(...)
+    local args = { ... }
+    return setmetatable({
+        call = function(v)
+            for _, i in pairs(args) do
+                if is_eq(i, v) then return true end
+            end
+            return false
+        end
+    }, switch.id)
+end
+
+local all_types = {
+    ["string"] = true,
+    ["number"] = true,
+    ["boolean"] = true,
+    ["table"] = true,
+    ["function"] = true,
+    ["thread"] = true,
+    ["userdata"] = true,
+    ["nil"] = true
+}
+function switch.typeof(t)
+    assert(type(t) == "string", "Type must be a string")
+    assert(all_types[t], "Invalid type: " .. t)
+    return setmetatable({
+        call = function(v)
+            return type(v) == t
+        end
+    }, switch.id)
+end
+
+function switch.range(min, max, step)
+    step = step or 1
+    assert(type(min) == "number", "Minimum must be a number")
+    assert(type(max) == "number", "Maximum must be a number")
+    assert(type(step) == "number", "Step must be a number")
+    assert(step > 0, "Step must be positive")
+    return setmetatable({
+        call = function(v)
+            if type(v) ~= "number" then return false end
+            if v < min or v > max then return false end
+            return (v - min) % step == 0
+        end
+    }, switch.id)
+end
+
+function switch.pattern(pattern)
+    assert(type(pattern) == "string", "Pattern must be a string")
+    return setmetatable({
+        call = function(v)
+            if type(v) ~= "string" then return false end
+            return v:match(pattern) ~= nil
+        end
+    }, switch.id)
+end
+
+function switch.new(_, v)
     return function(t)
         local val
-
-        if type(v) == "table" then
-            if t[v] then
-                val = t[v]
+        for k, o in pairs(t) do
+            if type(k) == "table" and getmetatable(k) == switch.id then
+                if k.call(v) then
+                    val = o; break
+                end
             else
-                for k, o in pairs(t) do
-                    if type(k) == "table" and tbl_eq(k, v) then
-                        val = o
-                        break
-                    end
+                if is_eq(k, v) then
+                    val = o; break
                 end
             end
-        else
-            val = t[v]
         end
-
         val = val or t.default
         return type(val) == "function" and val() or val
     end
 end
+
+return setmetatable(switch, { __call = switch.new })
